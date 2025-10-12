699cb08703b2ee830701c58621122e1b513a87c3
ojhankit <ojha27ankit@gmail.com>
2025-10-12 18:17:40 +0000

    app

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,28 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Features from './components/Features';
 import Workflow from './components/Workflow';
+import Reviews from './components/Reviews';
+import Subscription from './components/Subscription';
+import FAQ from './components/FAQ';
+import Plans from './components/Plans';
+import Footer from './components/Footer';
+import News from './components/News';
 
 const App = () => {
   return (
-    <div className='app min-h-screen w-full radial-gradient-bg text-white flex flex-col items-center'>
-        <Navbar />
-        <Hero/>
+    <div className='app min-h-screen w-full text-white flex flex-col items-center'>
+        <div className='radial-gradient-bg w-full flex flex-col items-center'>
+            <Navbar />
+            <Hero/>
+        </div>
         <Features/>
         <Workflow />
+        <Plans/>
+        <News/>
+        <Reviews/>
+        <FAQ/>
+        <Subscription/>
+        <Footer/>
     </div>
   )
 }
